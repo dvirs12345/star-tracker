03bcd4beca9685328a21a4149a9099fb6e44901a
dvir <dvirs120@gmail.com>
2021-06-12 11:44:19 +0300

    First commit

---
+++ rotationDetection.py
@@ -0,0 +1,123 @@
+import numpy as np
+import cv2
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import math
+
+SValue = 12000000
+
+
+def calc_distance(x1, x2, y1, y2):
+    return math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))  # Distance between two points formula
+
+
+def get_distances(df, myid):
+    mystar = df[df["id"] == myid]
+    others = df[df["id"] != myid]
+    distdict = {}
+    for index, row in others.iterrows():
+        distdict[row["id"]] = calc_distance(row['x'], mystar['x'], row['y'], mystar['y'])
+
+    print(distdict)
+    return distdict
+
+
+def update_distance(df):
+    df["Pdistances"] = None
+    for index, row in df.iterrows():
+        df["Pdistances"][index] = get_distances(df, row["id"])
+
+    return df
+
+
+def load_detection_result(filename):
+    """ Loads the data and returns the df with all the info """
+    file1 = open(filename, 'r')
+    Lines = file1.readlines()
+
+    df = pd.DataFrame(None, columns=['x', 'y', 'type'])
+    for line in Lines:
+        split1 = line.split('\n')  # Remove \n
+        split2 = split1[0].split(",")
+        for i in range(len(split2)):  # Remove whitespaces and turns string into float
+            split2[i] = float(split2[i].strip())
+
+        myseries = pd.Series(split2, index=df.columns)
+        df = df.append(myseries, ignore_index=True)
+
+    df["id"] = range(df.shape[0])
+
+    return df
+
+
+def load_star_data(filename, max_visible):
+    mycsv = pd.read_csv(filename)
+    mycsv = mycsv[["id", "ra", "proper", "dec", "x", "y", "z", "mag"]]  # Have only the vars for distance calculation
+    mycsv = mycsv[mycsv["mag"] <= max_visible]  # Remove the stars that we can't see with the naked eye = 6
+    mycsv = mycsv[mycsv["id"] != 0]  # Remove the sun
+    print(mycsv.head())
+    print(mycsv.shape)
+    return mycsv
+
+
+def calc_AD(stardf, id1, id2):
+    dec1 = stardf.loc[stardf["id"] == id1, "dec"]
+    dec2 = stardf.loc[stardf["id"] == id2, "dec"]
+    ra1 = stardf.loc[stardf["id"] == id1, "ra"]
+    ra2 = stardf.loc[stardf["id"] == id2, "ra"]
+
+    return math.sin(dec1) * math.sin(dec2) + math.cos(dec1) * math.cos(dec2) * math.cos(ra1 - ra2)  # formula for AD
+
+
+def calc_SPD(S, AD):
+    return S * AD
+
+
+def update_angular_distance(stardf):
+    stardf["pdistances"] = None
+    for index, row in stardf.iterrows():
+        stardf["pdistances"][index] = get_star_distances(stardf, row['id'])
+
+    return stardf
+
+
+def get_star_distances(stardf, myid):
+    mystar = stardf[stardf["id"] == myid]
+    others = stardf[stardf["id"] != myid]
+    distdict = {}
+    for index, row in others.iterrows():
+        distdict[row["id"]] = calc_SPD(SValue, calc_AD(stardf, myid, row["id"]))
+
+    return distdict
+
+
+def search_distances(stardf, dist):
+    possiblities = []
+    for index, row in stardf.iterrows():
+        temp = row["pdistances"]
+        for key in temp:
+            if temp[key] == dist:
+                possiblities.append(key)
+
+    return possiblities
+
+
+def main():
+    mypic1 = cv2.imread('pic1.jpg')
+
+    Original_Image = Image.open("pic1.jpg")
+
+    rotated_image1 = Original_Image.rotate(90)
+    plt.imshow(rotated_image1)
+    # plt.show()
+    rotated_image1.save("pic1r.jpg")
+
+    df = load_detection_result("pic1.jpg_res.txt")
+    df = update_distance(df)
+    # print(df.head())
+
+    load_star_data("hygdata_v3.csv", 6)
+
+
+main()
---
+++ starDetector.py
@@ -0,0 +1,98 @@
+import numpy as np
+import argparse
+import cv2
+import math
+
+
+# if you want to read from CMD
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", help = "path to the image file")
+# args = vars(ap.parse_args())
+# image = cv2.imread('fullExStarS9.jpg')
+
+
+def takeRadius(elem):
+    return float(elem[2])
+
+
+def takeX(elem):
+    return float(elem[0])
+
+
+# file_name='/Users/revital/Downloads/120.png'
+file_name = 'pic1.jpg'
+
+file = open("%s_res.txt" % file_name, "w")
+image = cv2.imread(file_name)
+orig = image.copy()
+imgheight, imgwidth = image.shape[:2]
+# image = image[0:2500,0:4000 ]
+# orig = image.copy()
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+# sobelx = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=11)
+# sobelx=sobelx*255
+edges = cv2.Canny(image, 70, 100)
+print(np.max(edges))
+print((edges.sum() / 255) / (imgheight * imgwidth))
+
+# print(edges)
+# print (edges)
+# cv2.circle(edges, maxLoc, 5, (255, 0, 0), 3)
+contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# print (contours)
+contour_list = []
+# print (contours)
+# 0area = cv2.contourArea(contours[0])
+res = []
+for contour in contours:
+    # approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
+    # print("in for")
+    area = cv2.contourArea(contour)
+    # print(area)
+    (x, y), radius = cv2.minEnclosingCircle(contour)
+
+    center = (int(x), int(y))
+    radius = int(radius)
+    xint = center[0]
+    yint = center[1]
+    x = str(x)
+    y = str(y)
+    r = str(radius)
+
+    if 1 < radius < 5:  # (and xint>300 and yint> 300):
+        cv2.circle(orig, center, radius + 4, (0, 255, 255), 5)
+        res.append([x, y, radius])
+# contour_list.append(contour)
+# ret, thresh = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+connectivity = 4
+# output = cv2.connectedComponentsWithStats(image, connectivity, cv2.CV_32S)
+# cv2.drawContours(image, contour_list,  -1, (255,255,0), 1)
+# display the results of the naive attempt
+cv2.imshow("Naive", orig)
+cv2.imwrite("%s_processed.jpg" % file_name, orig)
+
+# res.sort(key=takeRadius,reverse=True )
+res.sort(key=takeX, reverse=False)
+# cv2.waitKey(0)
+counter = 0
+final_res = []
+print(res)
+file.write("%s ,%s ,%s \n" % (res[0][0], res[0][1], res[0][2]))
+final_res.append([float(res[0][0]), float(res[0][1])])
+for i in range(1, len(res)):
+    if counter < 10 and math.fabs(float(res[i][0]) - float(res[i - 1][0])) > 10:
+        file.write("%s ,%s ,%s \n" % (res[i][0], res[i][1], res[i][2]))
+        final_res.append([float(res[i][0]), float(res[i][1])])
+        counter = counter + 1
+
+        # file.write(t)
+    # file.write('\n')
+file.close()
+
+for i in range(len(final_res)):
+    for j in range(i + 1, len(final_res)):
+        x = final_res[i][0] - final_res[j][0]
+        y = final_res[i][1] - final_res[j][1]
+        d = math.sqrt(x * x + y * y)
+        print(str(d))
